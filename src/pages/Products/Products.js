25304fc1ca04d012e67cc6eaddbcb1b10042ad53
bilalseen <bilalsen2010@gmail.com>
2024-01-09 01:55:13 +0300

    commit message

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,11 @@
 import { SafeAreaView, Text, TouchableOpacity, FlatList } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import styles from './Products.style'
+import LottieView from 'lottie-react-native'
+
 
 const Products = ({ navigation }) => {
-
+    const [loading, setLoading] = useState(true)
     const [data, setData] = useState(null)
 
     const getProducts = async () => {
@@ -14,14 +16,23 @@ const Products = ({ navigation }) => {
             }
             const dataProduct = await res.json()
             setData(dataProduct);
+            setLoading(false)
         } catch (err) {
             alert("Error message: " + err.message)
+            setLoading(false)
         }
     }
 
     useEffect(() => {
         getProducts();
     }, [])
+
+    if (loading) {
+        return (
+            <LottieView source={require("../../../assets/loading.json")} autoPlay loop />
+        )
+    }
+
 
 
     return (
