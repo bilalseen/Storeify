a18ad8014f4f18cdc65ce84742b6f72c4a10b34e
bilalseen <bilalsen2010@gmail.com>
2024-01-09 02:10:15 +0300

    custom hook useFetch created

---
+++ src/hooks/useFetch/useFetch.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+
+export default function useFetch(url) {
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [data, setData] = useState(null)
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url)
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            const responseData = await response.json()
+            setData(responseData);
+            setLoading(false)
+        } catch (err) {
+            setError(err.message)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    return { loading, data };
+}
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -2,30 +2,11 @@ import { SafeAreaView, Text, TouchableOpacity, FlatList } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import styles from './Products.style'
 import LottieView from 'lottie-react-native'
+import useFetch from '../../hooks/useFetch/useFetch'
 
 
 const Products = ({ navigation }) => {
-    const [loading, setLoading] = useState(true)
-    const [data, setData] = useState(null)
-
-    const getProducts = async () => {
-        try {
-            const res = await fetch("https://fakestoreapi.com/products")
-            if (!res.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const dataProduct = await res.json()
-            setData(dataProduct);
-            setLoading(false)
-        } catch (err) {
-            alert("Error message: " + err.message)
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        getProducts();
-    }, [])
+    const { loading, data } = useFetch("https://fakestoreapi.com/products");
 
     if (loading) {
         return (
