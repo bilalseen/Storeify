2e455b29e9c115a2fb5822a8ae33a7ff754dc525
bilalseen <bilalsen2010@gmail.com>
2024-01-09 01:03:20 +0300

    Product component created

---
+++ src/components/Product/Product.js
@@ -0,0 +1,17 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import styles from "./Product.style"
+
+const Product = () => {
+    return (
+        <View>
+            <Image />
+            <Text>Title</Text>
+            <Text>Desc</Text>
+            <Text>Price</Text>
+        </View>
+    )
+}
+
+export default Product
+
