7caea3291497e86e1964855dca7253c7b7fc9212
bilalseen <bilalsen2010@gmail.com>
2024-01-09 20:28:52 +0300

    custom hooks created

--- src/hooks/useFetch/useFetch.js
+++ src/hooks/useFetch/useFetch.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from "react";
 
-
 export default function useFetch(url) {
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [data, setData] = useState(null)
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch(url)
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const responseData = await response.json()
-            setData(responseData);
-            setLoading(false)
-        } catch (err) {
-            setError(err.message)
-            setLoading(false)
-        }
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const responseData = await response.json();
+      setData(responseData);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return { loading, data };
+  return { loading, data, error };
 }
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,26 +1,44 @@
-import { SafeAreaView, Text, TouchableOpacity, FlatList } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import styles from './Products.style'
-import LottieView from 'lottie-react-native'
-import useFetch from '../../hooks/useFetch/useFetch'
-
+import { SafeAreaView, Text, TouchableOpacity, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import styles from "./Products.style";
+import LottieView from "lottie-react-native";
+import useFetch from "../../hooks/useFetch/useFetch";
+import Product from "../../components/Product";
 
 const Products = ({ navigation }) => {
-    const { loading, data } = useFetch("https://fakestoreapi.com/products");
+  const [roductsApi, setProductsApi] = useState(
+    "https://fakestoreapi.com/products"
+  );
+  const { loading, data, error } = useFetch(roductsApi);
 
-    if (loading) {
-        return (
-            <LottieView source={require("../../../assets/loading.json")} autoPlay loop />
-        )
-    }
-
-
-
+  if (loading) {
     return (
-        <SafeAreaView style={styles.container}>
-            <FlatList data={data} renderItem={({ item }) => <Text>{item.title}</Text>} />
-        </SafeAreaView>
-    )
-}
+      <LottieView
+        source={require("../../../assets/loading.json")}
+        autoPlay
+        loop
+      />
+    );
+  }
 
-export default Products
+  if (error) {
+    return (
+      <LottieView
+        source={require("../../../assets/error.json")}
+        autoPlay
+        loop
+      />
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => <Product product={item} />}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Products;
