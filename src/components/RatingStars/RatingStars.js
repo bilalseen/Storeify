4ac36b44fe8a0176f2bcd60bf4ccbedc6bf93746
bilalseen <bilalsen2010@gmail.com>
2024-01-12 22:22:42 +0300

    RatingStars component created

---
+++ src/components/RatingStars/RatingStars.js
@@ -0,0 +1,13 @@
+import { SafeAreaView, View, Text } from "react-native";
+import React from "react";
+import styles from "./RatingStars.style";
+
+const RatingStars = () => {
+  return (
+    <View>
+      <Text>RatingStars</Text>
+    </View>
+  );
+};
+
+export default RatingStars;
