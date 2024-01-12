ea403f1c5fcb5fc3a9d65c44b1998f9e68825fe8
bilalseen <bilalsen2010@gmail.com>
2024-01-12 21:34:16 +0300

    Detail page style edited

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,13 +1,48 @@
-import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
-import styles from './Detail.style'
+import { SafeAreaView, Text, Image, View } from "react-native";
+import React from "react";
+import styles from "./Detail.style";
+import useFetch from "../../hooks/useFetch/useFetch";
+import LottieView from "lottie-react-native";
 
-const Detail = () => {
+const Detail = ({ route }) => {
+  const { id } = route.params;
+  const { loading, data, error } = useFetch(
+    `https://fakestoreapi.com/products/${id}`
+  );
+  console.log(data);
+  if (error) {
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Detail Page!!!</Text>
-        </SafeAreaView>
-    )
-}
+      <LottieView
+        source={require("../../../assets/error.json")}
+        autoPlay
+        loop
+      />
+    );
+  }
 
-export default Detail
+  if (loading) {
+    return (
+      <LottieView
+        source={require("../../../assets/loading.json")}
+        autoPlay
+        loop
+      />
+    );
+  }
+
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <Image source={{ uri: data["image"] }} style={styles.productImage} />
+      <View style={styles.productDetailsContainer}>
+        <View style={styles.titleAndRatingContainer}>
+          <Text style={styles.productTitle}>{data["title"]}</Text>
+          <Text style={styles.productRating}>{data["rating"]["rate"]}</Text>
+        </View>
+        <Text style={styles.productDescription}>{data["description"]}</Text>
+        <Text style={styles.productPrice}>$ {data["price"]}</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Detail;
--- src/pages/Detail/Detail.style.js
+++ src/pages/Detail/Detail.style.js
@@ -1,7 +1,45 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-export default styles = StyleSheet.create({
-    container: {
-        flex: 1, justifyContent: "center", alignItems: "center"
-    }
-})
+const deviceSize = Dimensions.get("window");
+
+const styles = StyleSheet.create({
+  mainContainer: {},
+  productImage: {
+    width: deviceSize.width,
+    height: deviceSize.height / 3,
+    resizeMode: "contain",
+    backgroundColor: "white",
+  },
+  productDetailsContainer: {
+    backgroundColor: "#fff",
+    paddingTop: 16,
+    paddingBottom: 40,
+    paddingHorizontal: 26,
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    gap: 20,
+  },
+  titleAndRatingContainer: {},
+  productTitle: {
+    fontSize: 22,
+    fontWeight: "600",
+    textAlign: "center",
+  },
+  productRating: {
+    textAlign: "center",
+  },
+  productDescription: {},
+  productPrice: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+});
+
+export default styles;
