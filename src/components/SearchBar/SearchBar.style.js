9f5cc64fb78963fd368f7ac1b33c343fe058a80f
bilalseen <bilalsen2010@gmail.com>
2024-01-13 17:16:05 +0300

    Added shading to Search Bar styles, changed background color to white

--- src/components/SearchBar/SearchBar.style.js
+++ src/components/SearchBar/SearchBar.style.js
@@ -6,7 +6,16 @@ export default styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 10,
-    borderColor: "black",
-    borderWidth: 1
+    borderColor: "#EFEFEF",
+    borderWidth: 1,
+    backgroundColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+    elevation: 5,
   },
 });
