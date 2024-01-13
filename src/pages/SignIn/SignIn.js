0ac59cea3c5bb9d2c73c5da60ee0d47835d039a9
bilalseen <bilalsen2010@gmail.com>
2024-01-13 21:37:05 +0300

    SignIn page added the Router.js

---
+++ src/pages/SignIn/SignIn.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import styles from "./SignIn.style";
+
+const SignIn = () => {
+  return (
+    <View>
+      <Text>SignIn Page!!!</Text>
+    </View>
+  );
+};
+
+export default SignIn;
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import TabNavigator from "./TabNavigator";
 import Detail from "../pages/Detail";
+import SignIn from "../pages/SignIn/SignIn";
 
 const Stack = createNativeStackNavigator();
+
 export default function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="TabNavigator"
-          component={TabNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="DetailScreen" component={Detail} />
+        {user ? (
+          <>
+            <Stack.Screen
+              name="TabNavigator"
+              component={TabNavigator}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="DetailScreen" component={Detail} />
+          </>
+        ) : (
+          <Stack.Screen name="SignInScreen" component={SignIn} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
