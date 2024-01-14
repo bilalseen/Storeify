c57d36463813862fd0cd1a7f2501e870a82085d5
bilalseen <bilalsen2010@gmail.com>
2024-01-14 21:10:53 +0300

    Add navigation function to navigate to SignInScreen

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableWithoutFeedback } from "react-native";
 import React from "react";
 import SignUpInputForm from "../../components/SignUpInputForm";
 import Button from "../../components/Button";
@@ -6,11 +6,17 @@ import GoogleAuthButton from "../../components/GoogleAuthButton";
 import WelcomeAnimation from "../../components/WelcomeAnimation";
 
 import styles from "./SignUp.style";
-const SignUp = () => {
+const SignUp = ({ navigation }) => {
+  const navigateToSignIn = () => {
+    navigation.navigate("SignInScreen");
+  };
   return (
     <View style={styles.container}>
       <WelcomeAnimation />
       <SignUpInputForm />
+      <TouchableWithoutFeedback onPress={navigateToSignIn}>
+        <Text>Do you have a account? Sign in</Text>
+      </TouchableWithoutFeedback>
       <Button />
       <GoogleAuthButton />
     </View>
