c79316c540e61d52742130d28a6e8a2cb79b7635
bilalseen <bilalsen2010@gmail.com>
2024-01-15 00:19:09 +0300

    styles edited

--- src/components/SignInInputForm/SignInInputForm.style.js
+++ src/components/SignInInputForm/SignInInputForm.style.js
@@ -1,3 +1,7 @@
 import { StyleSheet } from "react-native";
 
-export default styles = StyleSheet.create({});
+export default styles = StyleSheet.create({
+  inputContainer: {
+    gap: 25,
+  },
+});
--- src/components/SignUpInputForm/SignUpInputForm.style.js
+++ src/components/SignUpInputForm/SignUpInputForm.style.js
@@ -3,5 +3,6 @@ import { StyleSheet } from "react-native";
 export default styles = StyleSheet.create({
   inputContainer: {
     alignItems: "center",
+    gap: 25,
   },
 });
