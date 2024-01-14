126cc3a89fb3ef8473a2ad2b8f5914c8e01a64ce
bilalseen <bilalsen2010@gmail.com>
2024-01-14 19:23:43 +0300

    Button, GoogleAuthButton, WelcomeAnimation components added the page

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,12 +1,18 @@
 import { View, Text } from "react-native";
 import React from "react";
 import UserInputForm from "../../components/UserInputForm";
+import Button from "../../components/Button";
+import GoogleAuthButton from "../../components/GoogleAuthButton";
+import WelcomeAnimation from "../../components/WelcomeAnimation";
 
 import styles from "./SignUp.style";
 const SignUp = () => {
   return (
-    <View>
+    <View style={styles.container}>
+      <WelcomeAnimation />
       <UserInputForm />
+      <Button />
+      <GoogleAuthButton />
     </View>
   );
 };
