2c88da020ec17b93c78c02481d6faa9a3bc3a1df
bilalseen <bilalsen2010@gmail.com>
2024-01-14 19:23:20 +0300

    style edited

--- src/pages/SignUp/SignUp.style.js
+++ src/pages/SignUp/SignUp.style.js
@@ -1,3 +1,9 @@
 import { StyleSheet } from "react-native";
 
-export default styles = StyleSheet.create({});
+export default styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    alignItems: "center",
+    gap: 20,
+  },
+});
