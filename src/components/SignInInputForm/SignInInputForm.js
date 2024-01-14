5038c42a307e1ea809a605d1a7bb8782a6623199
bilalseen <bilalsen2010@gmail.com>
2024-01-14 23:09:33 +0300

    signInInputForm created

---
+++ src/components/SignInInputForm/SignInInputForm.js
@@ -0,0 +1,33 @@
+import { View } from "react-native";
+import React from "react";
+import { Formik } from "formik";
+import CustomTextInput from "../CustomTextInput";
+
+import styles from "./SignInInputForm.style";
+
+const SignInInputForm = () => {
+  return (
+    <Formik initialValues={{ email: "", password: "" }} onSubmit={() => null}>
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <CustomTextInput
+            onChangeText={handleChange("email")}
+            onBlur={handleBlur("email")}
+            value={values.email}
+            placeholder={"Email "}
+            keyboardType={"email-address"}
+          />
+          <CustomTextInput
+            onChangeText={handleChange("password")}
+            onBlur={handleBlur("password")}
+            value={values.password}
+            placeholder={"Password"}
+            secureTextEntry={true}
+          />
+        </View>
+      )}
+    </Formik>
+  );
+};
+
+export default SignInInputForm;
