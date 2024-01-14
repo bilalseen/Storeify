45584a73ceca43198a9d7b0aad81dbc566593135
bilalseen <bilalsen2010@gmail.com>
2024-01-15 00:51:43 +0300

    Welcome naimation, sign in input form and navigate button added the sign in page

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,11 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableWithoutFeedback } from "react-native";
 import React from "react";
+import SignInInputForm from "../../components/SignInInputForm";
+import WelcomeAnimation from "../../components/WelcomeAnimation";
+
 import styles from "./SignIn.style";
 
-const SignIn = () => {
+const SignIn = ({ navigation }) => {
+  const navigateToSignUp = () => {
+    navigation.navigate("SignUpScreen");
+  };
   return (
-    <View>
-      <Text>SignIn Page!!!</Text>
+    <View style={styles.container}>
+      <WelcomeAnimation />
+      <SignInInputForm />
+      <TouchableWithoutFeedback onPress={navigateToSignUp}>
+        <Text style={styles.text}>Don't have an account? Become a member</Text>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
