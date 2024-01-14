d64b069e66e14ebb5e81d12ac5956778af911311
bilalseen <bilalsen2010@gmail.com>
2024-01-14 20:39:24 +0300

    styles added

--- src/components/WelcomeAnimation/WelcomeAnimation.js
+++ src/components/WelcomeAnimation/WelcomeAnimation.js
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 import LottieView from "lottie-react-native";
+import styles from "./WelcomeAnimation.style";
 
 const WelcomeAnimation = () => {
   return (
     <LottieView
+      style={styles.animationContainerStyle}
       source={require("../../../assets/welcome-animation.json")}
       autoPlay
       loop
---
+++ src/components/WelcomeAnimation/WelcomeAnimation.style.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  animationContainerStyle: {
+    height: 200,
+    width: 200,
+  },
+});
