c3356a9b9f210f3a4d5df4f4ab2b1d474b546dc9
bilalseen <bilalsen2010@gmail.com>
2024-01-14 17:18:02 +0300

    style edited

--- src/components/CustomTextInput/CustomTextInput.style.js
+++ src/components/CustomTextInput/CustomTextInput.style.js
@@ -1,3 +1,16 @@
 import { StyleSheet } from "react-native";
 
-export default styles = StyleSheet.create({});
+export default styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  input: {
+    width: 300,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: "#ddd",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+});
