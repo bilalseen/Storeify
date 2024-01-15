919fb663187790b3c86b7c08a787a0b1a4e168bb
bilalseen <bilalsen2010@gmail.com>
2024-01-15 10:31:02 +0300

    onSubmit prop added

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,9 +2,9 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import React from "react";
 import styles from "./Button.style";
 
-const Button = ({ text }) => {
+const Button = ({ text, onSubmit }) => {
   return (
-    <TouchableOpacity style={styles.buttonContainer}>
+    <TouchableOpacity style={styles.buttonContainer} onPress={onSubmit}>
       <Text style={styles.text}>{text}</Text>
     </TouchableOpacity>
   );
