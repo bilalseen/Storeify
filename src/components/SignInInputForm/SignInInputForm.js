704c22552c5d3d1a26268c20f5cb019b7edc01f6
bilalseen <bilalsen2010@gmail.com>
2024-01-15 17:35:44 +0300

    auth processing connected with firebase

--- src/components/SignInInputForm/SignInInputForm.js
+++ src/components/SignInInputForm/SignInInputForm.js
@@ -4,12 +4,31 @@ import { Formik } from "formik";
 import CustomTextInput from "../CustomTextInput";
 import Button from "../Button";
 import GoogleAuthButton from "../GoogleAuthButton";
+import FIREBASE_AUTH from "../../services/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 import styles from "./SignInInputForm.style";
 
+const signInUser = (values) => {
+  const auth = FIREBASE_AUTH;
+  signInWithEmailAndPassword(auth, values.email, values.password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
 const SignInInputForm = () => {
+  const onSubmit = (values) => {
+    signInUser(values);
+  };
   return (
-    <Formik initialValues={{ email: "", password: "" }} onSubmit={() => null}>
+    <Formik initialValues={{ email: "", password: "" }} onSubmit={onSubmit}>
       {({ handleChange, handleBlur, handleSubmit, values }) => (
         <View style={styles.inputContainer}>
           <CustomTextInput
@@ -26,7 +45,7 @@ const SignInInputForm = () => {
             placeholder={"Password"}
             secureTextEntry={true}
           />
-          <Button text={"Sign up"} />
+          <Button text={"Sign up"} onSubmit={handleSubmit} />
           <GoogleAuthButton />
         </View>
       )}
--- src/components/SignUpInputForm/SignUpInputForm.js
+++ src/components/SignUpInputForm/SignUpInputForm.js
@@ -4,14 +4,34 @@ import { Formik } from "formik";
 import CustomTextInput from "../CustomTextInput";
 import Button from "../Button";
 import GoogleAuthButton from "../../components/GoogleAuthButton";
+import FIREBASE_AUTH from "../../services/config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 import styles from "./SignUpInputForm.style";
 
+const signUpUser = (values) => {
+  const auth = FIREBASE_AUTH;
+  createUserWithEmailAndPassword(auth, values.email, values.password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+};
+
 const SignUpInputForm = () => {
+  const onSubmit = (values) => {
+    signUpUser(values);
+  };
   return (
     <Formik
       initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
-      onSubmit={() => null}
+      onSubmit={onSubmit}
     >
       {({ handleChange, handleBlur, handleSubmit, values }) => (
         <View style={styles.inputContainer}>
@@ -41,7 +61,7 @@ const SignUpInputForm = () => {
             placeholder={"Password"}
             secureTextEntry={true}
           />
-          <Button text={"Sign Up"} />
+          <Button text={"Sign Up"} onSubmit={handleSubmit} />
           <GoogleAuthButton />
         </View>
       )}
