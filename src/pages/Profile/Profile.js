1ecb4063e9390d07e8fc6029a745de92cf05af63
bilalseen <bilalsen2010@gmail.com>
2024-01-20 18:52:45 +0300

    Implement Log Out button using AuthManager

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,10 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import styles from "./Profile.style";
+import AuthManager from "../../services/AuthManager";
+import Button from "../../components/Button";
 const Profile = () => {
+  const { user, handleSignOut } = AuthManager();
   return (
     <View style={styles.mainContainer}>
       <Text>Profile Page!!!!</Text>
+      <Button text={"Log out"} onSubmit={handleSignOut} />
     </View>
   );
 };
