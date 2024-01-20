77c2232d85b518cd7167d6323fd0d1097571142f
bilalseen <bilalsen2010@gmail.com>
2024-01-20 18:52:31 +0300

    Add sign-out functionality to AuthManager

--- src/services/AuthManager.js
+++ src/services/AuthManager.js
@@ -1,14 +1,27 @@
+// AuthManager.js
+
 import { useState } from "react";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import FIREBASE_AUTH from "./config";
 
 const AuthManager = () => {
   const [user, setUser] = useState(null);
+
+  const handleSignOut = () => {
+    signOut(FIREBASE_AUTH)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.error("Çıkış işlemi başarısız oldu", error);
+      });
+  };
+
   onAuthStateChanged(FIREBASE_AUTH, (authUser) => {
     setUser(authUser);
   });
 
-  return user;
+  return { user, handleSignOut };
 };
 
 export default AuthManager;
